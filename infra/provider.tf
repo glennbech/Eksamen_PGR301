ef7bc0b2cd1c5e9aac6ea15cb3955d394392d268
smeepz <stianlw@gmail.com>
2021-11-25 00:19:23 +0100

    Added terraform code, there will be a bug here.

--- infra/provider.tf
+++ infra/provider.tf
@@ -5,4 +5,9 @@ terraform {
       version = "3.56.0"
     }
   }
+   backend "s3" {
+     bucket = "pgr301-stwe001-terraform"
+     key    = "stwe001-terraform.tfstate"
+     region = "eu-west-1"
+   }
 }
